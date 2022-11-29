919df923c5da116f5e6778d607e3afe7c41cc889
Miguel Antonio Bastidas Verdin <miguel.bastidas18@tectijuana.edu.mx>
2022-11-28 20:41:44 -0800

    .

--- store_frontend/organika/src/pages/about.jsx
+++ store_frontend/organika/src/pages/about.jsx
@@ -1,9 +1,10 @@
 const About = () => {
     return (
-        <div>
-        <h1>About</h1>
+        <div className="about">
+            <h4>By: Miguel Bastidas</h4>
+            <h5>Email: miguel.bastidas18@tectijuana.edu.mx</h5>
         </div>
     );
-    };
+};
 
 export default About;
--- store_frontend/organika/src/pages/admin.jsx
+++ store_frontend/organika/src/pages/admin.jsx
@@ -2,19 +2,38 @@ import './admin.css'
 import { useState } from 'react';
 const Admin = () => {
     const [product, setProducts] = useState({});
+    const [allProducts, setAllProducts] = useState([]);
+    const [couponCode, setCouponCode] = useState({});
+    const [allCoupons, setAllCoupons] = useState([]);
     const saveProduct = () => {
         console.log(product);
+        //add the products to the allProducts array
+        let copy = [...allProducts];
+        copy.push(product);
+        setAllProducts(copy);
     }
     const saveCoupon = () => {
-        console.log('save coupon');
+        console.log(couponCode);
+        //add the coupon to the allCoupon array
+        let copy = [...allCoupons];
+        copy.push(couponCode);
+        setAllCoupons(copy);
     }
     const productValChange = (e) => {
         let name = e.target.name;
         let value = e.target.value;
         //copy, modify the copy, set the copy back
-        let copy = {...product};
+        let copy = { ...product };
         copy[name] = value;
         setProducts(copy);
+    }
+    const couponValChange = (e) => {
+        let name = e.target.name;
+        let value = e.target.value;
+        //copy, modify the copy, set the copy back
+        let copy = { ...couponCode };
+        copy[name] = value;
+        setCouponCode(copy);
     }
     return (
         <div className="admin">
@@ -41,32 +60,48 @@ const Admin = () => {
                     <div className="my-control">
                         <button onClick={saveProduct} className="btn btn-dark">Save product</button>
                     </div>
+                    <hr />
+                    <h5>Product List</h5>
+                    <ul>
+                        {allProducts.map(p => (
+                            <li key={p.title}>
+                                {p.title} - ${parseFloat(p.price).toFixed(2)}
+                            </li>
+                        ))}
+                    </ul>
                 </div>
-
                 <div className="coupons-form">
                     <h5>Register Coupon Codes</h5>
-
                     <div className="my-control">
-                        <label htmlFor="">Coupon</label>
-                        <input type="text" />
-
+                        <label htmlFor="" >Coupon</label>
+                        <input name="code" type="text" onBlur={couponValChange} />
                     </div>
-
                     <div className="my-control">
                         <label htmlFor="">Discount</label>
-                        <input type="text" />
+                        <input name="discount" type="number" onBlur={couponValChange} />
                     </div>
-
-                    <div className="my-control">
+                    <div className="my-control center">
                         <button onClick={saveCoupon} className="btn btn-dark">Save Coupon</button>
                     </div>
-
+                    <hr />
+                    <h5>Valid Coupon Codes</h5>
+                    <ul>
+                        {allCoupons.map((c) => (
+                            <li key={c.code}>
+                                {c.code} - ${c.discount}
+                            </li>
+                        ))}
+                    </ul>
                 </div>
-
             </div>
         </div>
 
     );
 };
+
+// create a coupon state variable
+// create a function to save the coupon
+//input fields on the coupon form will call your couponValChange
+// whan the user click on the save coupon button, console log counpon state variable
 
 export default Admin;
